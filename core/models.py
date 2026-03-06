ce5b66dda07735bdd6e7dbd9378cac30105bd8ee
kirill <kirinkmitko@gmail.com>
2026-03-06 17:42:26 +0300

    refactor: :construction: Вынес группы в отдельную таблицу
    Вынес группы в отдельную таблицу, упростил запросы в бд, унифицировал названия переменных и типы данных

--- core/models.py
+++ core/models.py
@@ -2,49 +2,93 @@ from pydantic import BaseModel
 from typing import Literal
 from datetime import datetime
 
+WEEK_DAYS = Literal[
+    "Понедельник",
+    "Вторник",
+    "Среда",
+    "Четверг",
+    "Пятница",
+    "Суббота"
+]
+
+STUDY_LEVELS = Literal[
+    "СПО",
+    "Бакалавриат",
+    "Магистратура"
+]
+
+ROLES = Literal["STUDENT", "TEACHER"]
+
+
 class UserProfile(BaseModel):
-    id: str
+    """
+    user_id: telegram id\n
+    username: telegram username\n
+    full_name: telegram name\n
+    group_id: ref. to group table or None\n
+    teacher_id: ref. on teachers table or None
+    """
+
+    user_id: str
     username: str
     full_name: str
-    year: int
-    group: str
-    is_teacher: int
+    group_id: int
     teacher_id: int
 
-class UserStudent(UserProfile):
-    is_teacher: Literal[0] = 0
-    teacher_id: Literal[None] = None
-
-    year: int
-    group: str
-
-class UserTeacher(UserProfile):
-    is_teacher: Literal[1] = 1
-    year: Literal[0] = 0
-    group: Literal[""] = ""
-
-    teacher_id: int | None = None
-
 class Teacher(BaseModel):
-    id: int
+    """
+    teacher_id: iternal base id or None\n
+    FIO: family name and subname of teacher
+    """
+    teacher_id: int | None = None
     FIO: str
 
 class Pair(BaseModel):
-    id: int
-    group: str
-    day: str
+    """
+    pair_id: iternal base id or None\n
+    day: name of weakday\n
+    time: ISO format\n
+    subject: pair subject\n
+    auditory: auditory number\n
+    week_type: numerator/denumerator\n
+    group_id: ref. on group table\n
+    teacher_id: ref. on teachers table
+    """
+
+    pair_id: int | None = None
+    day: WEEK_DAYS
     time: datetime
     subject: str
     auditory: str
     week_type: Literal["NUMERATOR", "DENUMERATOR"]
-    teacher: str
+    group_id: int
+    teacher_id: int
+
+class StudentPair(Pair):
+    teacher_fio: str
+
+class TeacherPair(Pair):
+    group_code: str
+    year: int
+
+class Group(BaseModel):
+    """
+    group_id: iternal group id or None\n
+    level: styding level\n
+    year: year of studing\n
+    code: speciality abbriviature and group num. e.g. ISAP2
+    """
+    group_id: int | None = None
+    level: STUDY_LEVELS
+    year: int
+    code: str
 
 class Shedule(BaseModel):
     numerator: list[Pair]
     dennumerator: list[Pair]
 
 class StudentShedule(Shedule):
-    owner: Literal["STUDENT", "TEACHER"] = "STUDENT"
+    owner: ROLES = "STUDENT"
 
 class TeacherShedule(Shedule):
-    owner: Literal["STUDENT", "TEACHER"] = "TEACHER"
+    owner: ROLES = "TEACHER"
--- database/db.py
+++ database/db.py
@@ -10,7 +10,8 @@ import core.errors as errors
 async def create_tables(db: DataBase):
     async with db.db_cursor() as cursor:
         await TeachersBase.create_table(cursor)
-        await UsersBase.create_table(cursor)
+        await GroupsBase.create_table(cursor)
+        await UsersBase.create_table(cursor) # создаёт ссылки на TeachersBase & GroupsBase!
         await ShedulesBase.create_table(cursor)
 
 class DataBase:
@@ -38,11 +39,18 @@ class TeachersBase(DataBase):
     @staticmethod
     async def create_table(cursor: aiosqlite.Cursor) -> None:
         await cursor.execute(
-            """CREATE TABLE IF NOT EXISTS teachers (
+            """
+            CREATE TABLE IF NOT EXISTS teachers (
                 teacher_id INTEGER PRIMARY KEY AUTOINCREMENT,
                 FIO TEXT
             )"""
         )
+
+    @staticmethod
+    async def add_teacher(cursor: aiosqlite.Cursor, teacher: models.Teacher) -> None:
+        await cursor.execute("INSERT OR IGNORE INTO teachers (FIO) VALUES (?)", (teacher.FIO,))
+        if not cursor.rowcount:
+            raise errors.TeacherAlreadyExistsError()
 
     @staticmethod
     async def get_teachers(cursor: aiosqlite.Cursor) -> list[models.Teacher]:
@@ -57,47 +65,44 @@ class UsersBase(DataBase):
     async def create_table(cursor: aiosqlite.Cursor) -> None:
         await cursor.execute("PRAGMA foreign_keys = ON")
         await cursor.execute(
-            """CREATE TABLE IF NOT EXISTS users (
+            """
+            CREATE TABLE IF NOT EXISTS users (
                 user_id TEXT PRIMARY KEY,
                 username TEXT,
                 name TEXT,
-                year INTEGER,
-                group_code TEXT,
-                is_teacher INTEGER,
-                teacher INTEGER,
+                group_id INTEGER,
+                teacher_id INTEGER,
 
-                FOREIGN KEY (teacher) REFERENCES teachers (teacher_id) 
-                ON DELETE SET NULL
+                FOREIGN KEY (teacher_id) REFERENCES teachers (teacher_id) ON DELETE SET NULL,
+                FOREIGN KEY (group_id) REFERENCES groups (group_id) ON DELETE SET NULL
             )"""
         )
 
     @staticmethod
-    async def create_profile(cursor: aiosqlite.Cursor, profile_data: models.UserStudent | models.UserTeacher) -> None:
+    async def create_profile(cursor: aiosqlite.Cursor, profile_data: models.UserProfile) -> None:
         await cursor.execute(
-            "INSERT OR IGNORE INTO users (user_id, username, name, year, group_code, is_teacher, teacher) \
-                VALUES (?, ?, ?, ?, ?, ?, ?)", (
-                    profile_data.id,
+            "INSERT OR IGNORE INTO users (user_id, username, name, group_id, teacher_id) \
+                VALUES (?, ?, ?, ?, ?)", (
+                    profile_data.user_id,
                     profile_data.username,
                     profile_data.full_name,
-                    profile_data.year,
-                    profile_data.group,
-                    profile_data.is_teacher,
+                    profile_data.group_id,
                     profile_data.teacher_id,
                 )
         )
+        if not cursor.rowcount:
+            raise errors.UserAlreadyExistsError()
 
     @staticmethod
-    async def update_profile(cursor: aiosqlite.Cursor, profile_data: models.UserStudent | models.UserTeacher) -> None:
+    async def update_profile(cursor: aiosqlite.Cursor, profile_data: models.UserProfile) -> None:
         await cursor.execute(
-            "UPDATE users SET username = ?, name = ?, year = ?, group_code = ?, is_teacher = ?, teacher = ? WHERE user_id = ?",
+            "UPDATE users SET username = ?, name = ?, group_id = ?, teacher_id = ? WHERE user_id = ?",
             (
                 profile_data.username,
                 profile_data.full_name,
-                profile_data.year,
-                profile_data.group,
-                profile_data.is_teacher,
+                profile_data.group_id,
                 profile_data.teacher_id,
-                profile_data.id,
+                profile_data.user_id,
             )
         )
         if not cursor.rowcount:
@@ -119,60 +124,93 @@ class ShedulesBase(DataBase):
 
     @staticmethod
     async def create_table(cursor: aiosqlite.Cursor) -> None:
+        await cursor.execute("PRAGMA foreign_keys = ON")
         await cursor.execute(
-            """CREATE TABLE IF NOT EXISTS shedules (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                group_code TEXT,
+            """
+            CREATE TABLE IF NOT EXISTS pairs (
+                pair_id INTEGER PRIMARY KEY AUTOINCREMENT,
                 day TEXT,
                 time TEXT,
                 subject TEXT,
                 auditory TEXT,
                 week_type TEXT,
-                teacher INTEGER,
+                group_id INTEGER,
+                teacher_id INTEGER,
 
-                UNIQUE(group_code, day, time, week_type),
-                FOREIGN KEY (teacher) REFERENCES teachers (teacher_id) ON DELETE CASCADE
+                UNIQUE(group_id, day, time, week_type),
+                FOREIGN KEY (teacher_id) REFERENCES teachers (teacher_id) ON DELETE CASCADE,
+                FOREIGN KEY (group_id) REFERENCES groups (group_id) ON DELETE CASCADE
             )"""
         )
 
     @staticmethod
     async def set_pair(cursor: aiosqlite.Cursor, pair: models.Pair) -> None:
         await cursor.execute(
-            "REPLACE INTO shedules (group_code, day, time, subject, auditory, week_type, teacher) \
-                VALUES (?, ?, ?, ?, ?, ?)", (
-                    pair.group,
+            "REPLACE INTO pairs (day, time, subject, auditory, week_type, group_id, teacher_id) \
+                VALUES (?, ?, ?, ?, ?, ?, ?)", (
                     pair.day,
                     pair.time.isoformat(),
                     pair.subject,
                     pair.auditory,
                     pair.week_type,
-                    pair.teacher,
+                    pair.group_id,
+                    pair.teacher_id,
                 )
         )
 
     @staticmethod
-    async def get_student_pairs(cursor: aiosqlite.Cursor, group_code: str) -> list[models.Pair]:
+    async def get_student_pairs(cursor: aiosqlite.Cursor, group_id: int) -> list[models.StudentPair]:
         await cursor.execute(
-            "SELECT group_code, day, time, subject, auditory, week_type, teachers.FIO AS teacher FROM shedules \
-            JOIN teachers ON shedules.teacher = teachers.teacher_id \
-            WHERE group_code = ?", (group_code,)
+            """
+            SELECT pairs.*, teachers.FIO AS teacher_fio FROM pairs
+            JOIN teachers ON pairs.teacher_id = teachers.teacher_id
+            WHERE pairs.group_id = ?
+            """, (group_id,)
         )
         pairs = await cursor.fetchall()
-        return TypeAdapter(list[models.Pair]).validate_python(pairs)
+        return TypeAdapter(list[models.StudentPair]).validate_python(pairs)
     
     @staticmethod
-    async def get_teacher_pairs(cursor: aiosqlite.Cursor, teacher_id: int) -> list[models.Pair]:
+    async def get_teacher_pairs(cursor: aiosqlite.Cursor, teacher_id: int) -> list[models.TeacherPair]:
         await cursor.execute(
-            "SELECT shedules.group_code, shedules.day, shedules.time, shedules.subject, \
-                     shedules.auditory, shedules.week_type, teachers.FIO as teacher FROM teachers \
-            JOIN shedules ON shedules.teacher = teachers.teacher_id \
-            WHERE teachers.teacher_id = ?", (teacher_id,)
+            """
+            SELECT pairs.*, groups.code AS group_code, groups.year FROM pairs
+            JOIN groups ON pairs.group_id = groups.group_id
+            WHERE pairs.teacher_id = ?
+            """, (teacher_id,)
         )
         pairs = await cursor.fetchall()
-        return TypeAdapter(list[models.Pair]).validate_python(pairs)
+        return TypeAdapter(list[models.TeacherPair]).validate_python(pairs)
     
+
+class GroupsBase(DataBase):
+
     @staticmethod
-    async def get_groups(cursor: aiosqlite.Cursor) -> list[dict[str, str]]:
-        '''Returns: group'''
-        await cursor.execute("SELECT group_code FROM shedules GROUP BY group_code")
-        return await cursor.fetchall()
+    async def create_table(cursor: aiosqlite.Cursor) -> None:
+        await cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS groups (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                level TEXT,
+                year INTEGER,
+                code TEXT,
+
+                UNIQUE(level, year, code)
+            )"""
+        )
+
+    @staticmethod
+    async def create_group(cursor: aiosqlite.Cursor, group: models.Group) -> None:
+        await cursor.execute(
+            "INSERT OR IGNORE INTO groups (level, year, code) VALUES (?, ?, ?)",
+            (group.level, group.year, group.code,)
+        )
+        if not cursor.rowcount:
+            raise errors.GroupAlreadyExistsError()
+        
+    @staticmethod
+    async def get_group(cursor: aiosqlite.Cursor, group_id: int) -> models.Group:
+        await cursor.execute("SELECT * FROM groups WHERE group_id = ?", (group_id,))
+        group = await cursor.fetchone()
+        if not group: raise errors.GroupNotFoundError()
+        return models.Group.model_validate(group)
